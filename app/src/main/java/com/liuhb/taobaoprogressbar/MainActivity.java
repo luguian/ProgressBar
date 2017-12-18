dd3f626608532522882982c0ecbc7e043215a158
luguian <paul@xingyuehuyu.com>
2017-12-18 21:59:41 +0800

    第一次提交

---
+++ app/src/main/java/com/liuhb/taobaoprogressbar/MainActivity.java
@@ -0,0 +1,70 @@
+package com.liuhb.taobaoprogressbar;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.liuhb.taobaoprogressbar.com.liuhb.taobaoprogressbar.view.CustomProgressBar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private CustomProgressBar mProgressBar ;
+    private CustomProgressBar mProgressBar2 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mProgressBar = (CustomProgressBar) findViewById(R.id.cpb_progresbar);
+        mProgressBar.setOnFinishedListener(new CustomProgressBar.OnFinishedListener() {
+            @Override
+            public void onFinish() {
+                Toast.makeText(MainActivity.this,"done!",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mProgressBar.setProgressDesc("剩余");
+        mProgressBar.setMaxProgress(1804);
+        mProgressBar.setProgressColor(Color.parseColor("#F6CB82"));
+        mProgressBar.setCurProgress(156);
+
+
+        mProgressBar2 = (CustomProgressBar) findViewById(R.id.cpb_progresbar2);
+        mProgressBar2.setOnAnimationEndListener(new CustomProgressBar.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd() {
+                Toast.makeText(MainActivity.this,"animation end!",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mProgressBar2.setProgressDesc("剩余");
+        mProgressBar2.setMaxProgress(100);
+        mProgressBar2.setProgressColor(Color.parseColor("#79aa6b"));
+        mProgressBar2.setCurProgress(80,4000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
